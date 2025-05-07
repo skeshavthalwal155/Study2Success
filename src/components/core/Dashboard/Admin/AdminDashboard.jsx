7d0a3f10e0dea39a95d3ecb074bf202364f002df
Keshavthalwal155 <Keshavthalwal155@gmail.com>
2025-05-07 21:22:48 +0530

    s

--- src/components/core/Dashboard/Admin/AdminDashboard.jsx
+++ src/components/core/Dashboard/Admin/AdminDashboard.jsx
@@ -33,13 +33,13 @@ const AdminDashboard = () => {
   console.log("user", userData)
   // console.log("category", category)
 
-  if (loading) {
-    return (
-      <div className='flex h-[calc(100vh-3.5rem)] w-full justify-center items-center'>
-        <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 dark:text-dark-richblack-500 text-light-richblack-500'></div>
-      </div>
-    )
-  }
+  // if (loading) {
+  //   return (
+  //     <div className='flex h-[calc(100vh-3.5rem)] w-full justify-center items-center'>
+  //       <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 dark:text-dark-richblack-500 text-light-richblack-500'></div>
+  //     </div>
+  //   )
+  // }
 
   const totalCourses = () => {
     if (!category) return 0;
@@ -59,27 +59,30 @@ const AdminDashboard = () => {
 
   return (
     <div className='mx-auto w-11/12 max-w-[1000px] py-10'>
-      <div>
-        <div className='mb-14 flex items-center justify-between'>
-          <h1 className='text-3xl font-medium dark:text-dark-richblack-5 text-light-richblack-5'>Welcome Admin,</h1>
-        </div>
+      <div className='mb-14 flex items-center justify-between'>
+        <h1 className='text-3xl font-medium dark:text-dark-richblack-5 text-light-richblack-5'>Welcome Admin,</h1>
       </div>
-      <div >
-        <div>
-          Total Users
-          {userData?.length}
-        </div>
-        <div>
-          Total Categories
-          {category?.length}
-        </div>
-        <div>
-          Total Courses
-          {totalCourses()}
-        </div>
-        <div>
-          Total Instructor
-          {totalInstructor()}
+      <div className="p-10 text-light-richblack-5 dark:text-dark-richblack-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="border rounded-xl p-6 py-15 shadow dark:bg-dark-richblack-700">
+            <p className="text-lg font-semibold">Total Users</p>
+            <p>{userData?.length}</p>
+          </div>
+
+          <div className="border rounded-xl p-6 py-15 shadow dark:bg-dark-richblack-700">
+            <p className="text-lg font-semibold">Total Categories</p>
+            <p>{category?.length}</p>
+          </div>
+
+          <div className="border rounded-xl p-6 py-15  shadow dark:bg-dark-richblack-700">
+            <p className="text-lg font-semibold">Total Courses</p>
+            <p>{totalCourses()}</p>
+          </div>
+
+          <div className="border rounded-xl p-6 shadow dark:bg-dark-richblack-700">
+            <p className="text-lg font-semibold">Total Instructors</p>
+            <p>{totalInstructor()}</p>
+          </div>
         </div>
       </div>
     </div>
