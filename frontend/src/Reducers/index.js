1038691f15dc4857ff8e90678bbb4722010c8985
Keshavthalwal155 <Keshavthalwal155@gmail.com>
2025-05-12 19:33:57 +0530

    s

--- frontend/src/Reducers/index.js
+++ frontend/src/Reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import profileReducer from "../slices/profileSlice";
 import cartReducer from "../slices/cartSlice.js";
 import courseReducer from "../slices/courseSlice.js";
 import loadingBarReducer from "../slices/loadingBarSlice.js";
 import viewCourseReducer from "../slices/viewCourseSlice.js";
 import themeSlice from "../slices/themeSlice.js";
 import authReducer from '../slices/authSlice.js'
+import profileReducer from '../slices/profileSlice.js'
 
 
 const rootReducer = combineReducers({
