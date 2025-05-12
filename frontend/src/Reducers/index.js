9fc66289b5bcd5259dbc152194d5931e84461e0f
Keshavthalwal155 <Keshavthalwal155@gmail.com>
2025-05-12 19:36:08 +0530

    s

--- frontend/src/Reducers/index.js
+++ frontend/src/Reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import cartReducer from "../slices/cartSlice.js";
-import courseReducer from "../slices/courseSlice.js";
-import loadingBarReducer from "../slices/loadingBarSlice.js";
-import viewCourseReducer from "../slices/viewCourseSlice.js";
-import themeSlice from "../slices/themeSlice.js";
+
 import authReducer from '../slices/authSlice.js'
 import profileReducer from '../slices/profileSlice.js'
-
+import cartReducer from '../slices/cartSlice.js'
+import loadingBarReducer from '../slices/loadingBarSlice.js'
+import courseReducer from '../slices/courseSlice.js'
+import viewCourseReducer from '../slices/viewCourseSlice.js'
+import themeSlice from '../slices/themeSlice.js'
 
 const rootReducer = combineReducers({
     auth: authReducer,
