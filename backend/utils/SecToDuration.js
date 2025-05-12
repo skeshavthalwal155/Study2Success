7c997effe4d0084ddd474cfe7d19589cc0072355
Keshavthalwal155 <Keshavthalwal155@gmail.com>
2025-05-12 18:49:38 +0530

    s

--- backend/utils/SecToDuration.js
+++ backend/utils/SecToDuration.js
@@ -1,18 +1,16 @@
-// Helper Function to convert total second to the duration format
-function convertSecondsToDuration(totalSecond) {
-    const hours = Math.floor(totalSecond / 3600)
-    const minutes = Math.floor((totalSecond % 3600) / 60)
-    const seconds = Math.floor((totalSecond % 3600) % 60)
+// src/backend/utils/secToDuration.js
+function convertSecondsToDuration(totalSeconds) {
+    const hours = Math.floor(totalSeconds / 3600)
+    const minutes = Math.floor((totalSeconds % 3600) / 60)
+    const seconds = Math.floor((totalSeconds % 3600) % 60)
 
-    if(hours>0){
+    if (hours > 0) {
         return `${hours}h ${minutes}m`
-    }else if(minutes>0){
-        return `${minutes > 0}m ${seconds}s`
-    }else{
+    } else if (minutes > 0) {
+        return `${minutes}m ${seconds}s`
+    } else {
         return `${seconds}s`
     }
 }
 
-module.exports = {
-    convertSecondsToDuration
-}
+module.exports = convertSecondsToDuration;
