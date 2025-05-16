a3f423671e17a025c929ff958d1dc65da664644f
Keshavthalwal155 <Keshavthalwal155@gmail.com>
2025-05-16 10:58:54 +0530

    s

--- frontend/src/components/core/Auth/OpenRoute.jsx
+++ frontend/src/components/core/Auth/OpenRoute.jsx
@@ -1,27 +1,14 @@
-import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
-import {jwtDecode} from 'jwt-decode'
 
 const OpenRoute = ({ children }) => {
   const { token } = useSelector((state) => state.auth)
 
   if (token === null) {
     return children
-  } 
-  try{
-    const decodedToken = jwtDecode(token)
-    const currentTime = Date.now()/1000
-
-    if(decodedToken.exp<currentTime){
-      return children
-    }
+  } else {
     return <Navigate to={'/dashboard/my-profile'} />
-  }catch(error){
-    return children
   }
-  
- 
 }
 
 export default OpenRoute
