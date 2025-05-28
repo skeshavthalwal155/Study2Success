17da64bd74a00262d22987e8ad8f9158ffc21219
Keshavthalwal155 <Keshavthalwal155@gmail.com>
2025-05-28 11:53:53 +0530

    s

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -1,9 +1,12 @@
-import signupImg from "../assets/Images/signup.webp"
+import signupLight from "../assets/Images/signupLight.png"
+import signupDark from "../assets/Images/signupDark.png"
 import Template from "../components/core/Auth/Template"
 import { useSelector } from "react-redux";
 
 const Signup = () => {
   const { loading } = useSelector((state)=>state.auth)
+  const theme = useSelector((state)=>state.theme)
+   
   return (
     loading ? (<div className='h-[100vh] flex justify-center items-center'><div className='custom-loader'></div></div>) : (
 
@@ -11,10 +14,11 @@ const Signup = () => {
         title="Join the millions learning to code with study2success for free"
         description1="Build skills for today, tomorrow, and beyond."
         description2="Education to future-proof your career."
-        image={signupImg}
+        image={theme === "light" ? signupLight : signupDark}
         formType="signup"
-
-      />
+        
+        />
+     
 
     )
   )
